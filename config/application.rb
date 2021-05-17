6c5a2e1aa744870c55d8493196af2b1e13376188
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-17 11:15:03 -0700

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module ApiSessionBackend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    config.session_store :cookie_store, key: '_app_session'
   end
 end
