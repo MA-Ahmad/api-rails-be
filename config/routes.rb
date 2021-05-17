e2bfdc63eb018e42d1dca743c7869d8d4485d2a7
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-17 11:15:00 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  
+  devise_for :users, path_prefix: "devise", controllers: { registrations: "registrations" }
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      devise_scope :user do
+        post "login" => "sessions#create", as: "login"
+        post "signup", to: "registrations#create", as: 'signup'
+        delete "logout" => "sessions#destroy", as: "logout"
+        put "password/update", to: "registrations#update_password"
+      end
+    end
+  end
+
   root "home#index"
   get '*path', to: 'home#index', via: :all
 end
