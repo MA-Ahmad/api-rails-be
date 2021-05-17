8aafa10dc51140899f3686869b676b021a51388a
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-17 11:14:58 -0700

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,30 +1,4 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    # allow do
-    #   origins "http://localhost:3000"
-    #   resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    # end
-  
-    # allow do
-    #   origins "https://auth-app-rails-back.herokuapp.com"
-    #   resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    # end
     allow do
       # origins '*'
       origins 'http://localhost:3000'
@@ -34,5 +8,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
         credentials: true
     end
+    Rails.application.config.action_controller.forgery_protection_origin_check = false
   end
   
